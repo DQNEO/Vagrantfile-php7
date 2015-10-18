6ad16073ce617b25b49a1c7f5d05d952cb7dd682
DQNEO <dqneoo@gmail.com>
2015-10-19 00:32:04 +0900

    build php7 on CentOS 7.1

---
+++ centos7.1/build-php7.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+set -e
+
+export LC_ALL=C
+export PHP_VER=7.0.0RC5
+
+sudo yum -y update
+sudo yum -y install epel-release
+sudo yum -y install tar make gcc autoconf bison re2c
+
+# install packages to build php extensions
+sudo yum -y install libxml2-devel libcurl-devel libmcrypt-devel openssl-devel libpng-devel
+
+mkdir ~/src
+cd ~/src/
+
+curl --location https://github.com/php/php-src/archive/php-${PHP_VER}.tar.gz -o php-${PHP_VER}.tar.gz
+tar xvfz php-${PHP_VER}.tar.gz
+cd php-src-php-${PHP_VER}/
+
+./buildconf --force
+./configure --prefix=/opt/php \
+            --enable-mbstring\
+            --enable-xml\
+            --enable-zip\
+            --enable-fpm\
+            --enable-ftp\
+            --enable-exif\
+            --enable-fileinfo\
+            --enable-pcntl\
+            --enable-pdo\
+            --with-curl\
+            --with-gd\
+            --with-curl\
+            --with-openssl\
+            --with-mcrypt\
+            --with-zlib\
+            --with-mysqli=mysqlnd\
+            --with-pdo-mysql=mysqlnd\
+            --enable-json\
+            --enable-phar\
+            --enable-cli
+
+make -j 2
+
+sudo make install
