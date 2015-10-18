a6d09de4cb449ba6a6d6c066f5141b2c8dd9c6ba
DQNEO <dqneoo@gmail.com>
2015-10-18 23:58:21 +0900

    mkdir only if not exist

--- ubuntu15.04/build-php7.sh
+++ ubuntu15.04/build-php7.sh
@@ -12,7 +12,7 @@ sudo apt-get install -y build-essential tar make gcc autoconf bison re2c
 sudo apt-get install -y libxml2-dev libmcrypt-dev  libpng12-dev
 sudo apt-get install -y libcurl4-openssl-dev pkg-config libssl-dev libsslcommon2-dev
 
-mkdir ~/src
+test -d ~/src || mkdir ~/src
 cd ~/src/
 
 curl --location https://github.com/php/php-src/archive/php-${PHP_VER}.tar.gz -o php-${PHP_VER}.tar.gz
